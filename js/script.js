a7436368c67c45a83fd49545d1f42cd219e67feb
Leira1998 <fer.leira@hotmail.com>
2016-10-04 19:49:35 -0400

    Page Update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
-/*var fade_intro = function (var $item, time) {
-  $item.hide();
-  $item.fadeIn(time);
-}*/
-
 var main = function(){
-  var $logo = $('.logo');
-  $logo.hide();
-
-  $('.header').click(function() {
-    $logo.fadeIn('slow');
-  });
+  
 }
 
 $(document).ready(main);
